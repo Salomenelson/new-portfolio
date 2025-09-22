109bc41d2c82f9a645dac97dbe5c069fb66b8dab
salome Nelson <salomenelson2002@gmail.com>
2025-09-22 12:58:07 +0100

    My Portfolio

--- src/components/Aboutcard.jsx
+++ src/components/Aboutcard.jsx
@@ -1,7 +1,7 @@
 
 export const Aboutcard = ({title, text, icon})=>{
     return(
-        <div className="w-[100%] text-white h-[20%] p-1 my-7 rounded-[7px] flex gap-2 shadow-[0_8px_20px_rgba(0,0,0,0.15)] hover:border-3 hover:border-[#312c51]">
+        <div className="w-[100%] text-white h-[20%] p-1 my-7 rounded-[7px] flex gap-2 shadow-[0_8px_20px_rgba(0,0,0,0.15)] border-3 border-[#48426d]">
             <div>
                 <img src={icon} alt="icon" className="w-[50px] my-2" />
             </div>
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,7 +5,7 @@ function Tech(){
             <div className="w-[80%] max-w-[1000px] my-0 mx-auto py-25">
                 <h2 className="text-[#f0c38e] pt-2 text-[2.4rem] text-center">Techstack</h2>
                 <p className="text-[1.5rem] text-center font-semibold pb-3">Experience with</p>
-                <div className="flex flex-wrap shadow-[0_8px_20px_rgba(0,0,0,0.15)] items-center justify-center">
+                <div className="flex flex-wrap shadow-[0_8px_20px_rgba(0,0,0,0.15)] items-center justify-center border-3 border-[#48426d]">
                     <img src="/html.png" alt="html icon" className="w-[130px] p-4" />
                     <img src="/CSS.png" alt="CSS icon" className="w-[110px] h-[110px] object-fit p-4"/>
                     <img src="/javascript.png" alt="javascript icon" className="w-[130px] p-4"/>
